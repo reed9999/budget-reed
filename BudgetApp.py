a186ed231133e298d1df3e328c9c2d857d37f32c
reed9999 <reed9999@uw.edu>
2018-02-27 17:35:07 -0800

    Tests pass (3 skips). Instance methods should have been in the inner __BudgetApp class. Added a set_rows() method.
    I'm using that singleton idiom (although it seems a bit clumsy). I don't understand why set_rows is necessary since I thought that getattr thing was supposed to pass through all the calls, including assignments, but perhaps assignments are the only kind that don't get passed through?

--- BudgetApp.py
+++ BudgetApp.py
@@ -11,6 +11,59 @@ class BudgetApp(object):
 
         def __str__(self):
             return repr(self)
+
+        def load_config(self):
+            self.config = {u"input-file" : "__/something.csv"}
+            return self.config
+
+        def read_data_from_input_file(self):
+            self.rows = []
+            with open(self.config[u"input-file"]) as f:
+                self.rows = list(csv.DictReader(f))
+
+        def output_data(self):
+            with open("__temp-output-file.csv", 'w') as f:
+                fieldnames = self.rows[0].keys()
+                w = csv.DictWriter(f, fieldnames)
+                w.writeheader()
+                w.writerows(self.rows)
+
+        def clean_up_data(self):
+            # There are probably cute more-Pythonic ways to do this with map.
+            # For now this is my attempt at a crude but working implementation.
+            for row in self.rows:
+                row['Date'] = self.parse_date(row['Date'])
+                # unchanged row['Description']
+                row['Amount'] = self.parse_gain_loss(row['Amount'])
+
+
+        #If I wanted to lay the groundwork for something cute and polymorphic, I could have
+        # references to each method for different formats, and then standardize the interface
+        # so as to just iterate through a dict matching each field with its format.
+        def parse_date(self, str):
+            return "XX"
+
+        def parse_gain_loss(self, str):
+            capture_digits_periods_commas = "([0-9\.,]*)"
+            optional_quote = '"*'
+            pattern = "\(" + optional_quote + capture_digits_periods_commas + optional_quote + "\)"
+            result = re.search(pattern, str)
+            if result:
+                print ("Fake it til you make it. Better improve the tests.")
+                return -1234.45
+            else:
+                return str
+
+        def run_for_instance(self):
+                self.load_config()
+                self.read_data_from_input_file()
+                self.clean_up_data()
+                self.output_data()
+                print(self.rows)
+
+        def set_rows(self, rows):
+            self.rows = rows
+
     #I don't understand why we wouldn't want to double-underscore this.
     __instance = None
 
@@ -25,53 +78,6 @@ class BudgetApp(object):
     def __getattr__(self, name):
         return getattr(self.__instance, name)
 
-    def load_config(self):
-        self.config = {u"input-file" : "__/something.csv"}
-        return self.config
-
-    def read_data_from_input_file(self):
-        self.rows = []
-        with open(self.config[u"input-file"]) as f:
-            self.rows = list(csv.DictReader(f))
-
-    def output_data(self):
-        with open("__temp-output-file.csv", 'w') as f:
-            fieldnames = self.rows[0].keys()
-            w = csv.DictWriter(f, fieldnames)
-            w.writeheader()
-            w.writerows(self.rows)
-
-    def clean_up_data(self):
-        #There are probably cute more-Pythonic ways to do this with map.
-        # For now this is my attempt at a crude but working implementation.
-        for row in self.rows:
-            row['Date'] = self.parse_date(row['Date'])
-            #unchanged row['Description']
-            row['Amount'] = self.parse_gain_loss(row['Amount'])
-
-    #If I wanted to lay the groundwork for something cute and polymorphic, I could have
-    # references to each method for different formats, and then standardize the interface
-    # so as to just iterate through a dict matching each field with its format.
-    def parse_date(self, str):
-        return "XX"
-
-    def parse_gain_loss(self, str):
-        capture_digits_periods_commas = "([0-9\.,]*)"
-        optional_quote = '"*'
-        pattern = "\(" + optional_quote + capture_digits_periods_commas + optional_quote + "\)"
-        result = re.search(pattern, str)
-        if result:
-            print ("Fake it til you make it. Better improve the tests.")
-            return -1234.45
-        else:
-            return str
-
-    def run_for_instance(self):
-        self.load_config()
-        self.read_data_from_input_file()
-        self.clean_up_data()
-        self.output_data()
-        print(self.rows)
 
     @classmethod
     def run(cls) -> object:
--- test_budgetApp.py
+++ test_budgetApp.py
@@ -1,21 +1,26 @@
 from unittest import TestCase
 from BudgetApp import BudgetApp
 
+
 class TestBudgetApp(TestCase):
     def setUp(self):
         self._app = BudgetApp()
 
     def test_load_config(self):
         config = self._app.load_config()
-        self.assertIsNotNone(config)
+        self.assertIsNotNone(self._app.config, 'Should have loaded a config [and returned a value].')
+        self.assertIsNotNone(config, 'Should have [loaded a config and] returned a value.')
+        self.assertIsNotNone(config['input-file'], 'Loaded config should include input-file')
 
     def test_read_data_from_input_file(self):
-        self._app.load_config()
-        self.assertIsNotNone(self._app.config)
+        self._app.read_data_from_input_file()
+        self.assertIsNotNone(self._app.rows)
+        self.assertGreater(len(self._app.rows), 0, 'Should have read in some rows from the input file.')
 
     def test_output_data(self):
         list_of_dicts = [{'hello' : 1, 'goodbye' : 2}]
-        self._app.rows = list_of_dicts
+        # self._app.__instance.rows = list_of_dicts
+        self._app.set_rows(list_of_dicts)
         self._app.output_data()
         fn = "__temp-output-file.csv"
         with open(fn, 'r') as f:
